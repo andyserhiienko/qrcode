d4dc639cd804b98dad666b0fe2708593c7296dd5
andy <andyserhiienko@gmail.com>
2025-06-04 13:54:25 +0300

    Create link processor veiw and css

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,7 +20,8 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/main/style.css',
+        'css/linkprocessor/style.css',
     ];
     public $js = [
     ];
